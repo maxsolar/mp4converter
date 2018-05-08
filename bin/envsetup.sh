19da0125d5163703402a788d8e988fe0c330b2f9
Jim T. Tang <Jim T. Tang>
2018-05-09 00:40:20 +0800

    add filename checker for spaces and comma(s)

--- bin/envsetup.sh
+++ bin/envsetup.sh
@@ -1,25 +1,41 @@
+## for Windows Subsystem for Linux only
 function disable_bell() {
     echo "set visualbell" > ~/.vimrc
     sudo sed -i 's/# set bell-style none/set bell-style none/' /etc/inputrc
 }
 
 function repoupdate() {
-    sudo sh -c "for i in xenial xenial-updates xenial-security xenial-proposed xenial-backports; do
+    RELEASE=$(lsb_release -sc)
+    sudo sh -c "for i in $RELEASE $RELEASE-updates $RELEASE-security $RELEASE-proposed $RELEASE-backports; do
     echo deb http://tw.archive.ubuntu.com/ubuntu \$i main restricted universe multiverse
     done > /etc/apt/sources.list"
     sudo apt-get update
 }
 
 function check_requirements() {
-   dpkg -l ffmpeg | grep ii 2>/dev/null >&2 || sudo apt-get install ffmpeg -y 
-   dpkg -l libav-tools |grep ii 2>/dev/null >&2 || sudo apt-get install libav-tools -y
+   dpkg -l "$@" | grep ii 2>/dev/null >&2 || sudo apt-get install "$@" -y 
 }
 
-function type_checker() {
+function check_filename() {
+    echo processing "$1"...
+    export tmp_filename=$(echo "$1"|sed 's/[, ]\+/_/g')
+    original_filename_checksum=$(echo "$1"|md5sum| awk '{print $1}')
+    tmp_filename_checksum=$(echo "$tmp_filename"|md5sum| awk '{print $1}')
+
+    if [ "$original_filename_checksum" != "$tmp_filename_checksum" ]; then
+       mv -v "$1" $tmp_filename
+    else
+       echo "$1" filename is OK.
+    fi
+}
+
+function check_suffix() {
     if [ "${1##*.}" == "mp4" ]; then
         export suffix="_resized.mp4"
     elif [ "${1##*.}" == "flv" ]; then
         export suffix="_converted.mp4"
+    elif [ "${1##*.}" == "wma" ]; then
+        export suffix=".mp3"
     else
         echo unknown type ."${1##*.}"; exit 1
     fi
